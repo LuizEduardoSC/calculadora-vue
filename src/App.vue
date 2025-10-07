ea91f468ce39c015b5e61475003f50b45c23e76b
LuizEduardoSC <luizeduardoedd1@gmail.com>
2025-10-06 23:23:37 -0300

    Criando calculadora com VueJS

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case '+':
+          return this.numero1 + this.numero2;
+        case '-':
+          return this.numero1 - this.numero2;
+        case '*':
+          return this.numero1 * this.numero2;
+        case '/':
+          return this.numero2 !== 0 ? this.numero1 / this.numero2 : 'Erro: divisão por zero';
+        default:
+          return 0;
+      }
+    },
+  },
+};
+
+</script>
+
+<template>
+  <div class="calculadora">
+    <h1>Calculadora Aritmética</h1>
+
+    <input type="number" v-model.number="numero1" placeholder="Número 1" />
+    <input type="number" v-model.number="numero2" placeholder="Número 2" />
+
+    <select v-model="operacao">
+      <option value="+">Adição (+)</option>
+      <option value="-">Subtração (-)</option>
+      <option value="*">Multiplicação (*)</option>
+      <option value="/">Divisão (/)</option>
+    </select>
+
+    <p>Resultado: {{ resultado }}</p>
+  </div>
+
+</template>
+
+<style scoped>
+.calculadora {
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+}
+
+input,
+select {
+  margin: 10px;
+  padding: 5px;
+}
+</style>
